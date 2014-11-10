731c674a1beb26bcbf2afbb88c990399cb52ef27
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2014-11-10 18:02:20 -0500

    added name and access token to the workers

--- src/hivewing_core/worker.clj
+++ src/hivewing_core/worker.clj
@@ -3,4 +3,6 @@
             [clojure.java.jdbc :as jdbc]
             [environ.core  :refer [env]]))
 
+;(jdbc/insert! sql-db :workers {:name "first worker"})
 ;(jdbc/query sql-db ["SELECT * FROM workers"])
+;(jdbc/update! sql-db :workers {:name "New name"} ["uuid = ?" "fc84da1e-692c-11e4-a377-5f03441b19f7"])
