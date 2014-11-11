c6350feeb69b44f7a70c1f8e95778437d16829a5
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2014-11-11 18:23:40 -0500

    uploaded to s3 bucket, private, woot.

--- project.clj
+++ project.clj
@@ -10,6 +10,10 @@
                  [ragtime "0.3.6"]    ; needs to be 0.3.6 - stored functions dont work in 0.3.7
                  ]
   :plugins [[lein-environ "1.0.0"]
-            [ragtime/ragtime.lein "0.3.6"]]
+            [ragtime/ragtime.lein "0.3.6"]
+            [s3-wagon-private "1.1.2"]]
   :ragtime {:migrations ragtime.sql.files/migrations}
+  :repositories [["hivewing-core" {:url "s3p://clojars.hivewing.io/hivewing-core/releases"
+                             :username "AKIAJFFI52CFHPRZIPJA"
+                             :passphrase "THLlPQGSstzspARpgw4s+LgqFT2DyrNFJ+vxkHDf"}]]
   )
