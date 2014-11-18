22b8c4a377b31c7da71e96e5fbeab40ec55ee44e
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2014-11-18 14:57:34 -0500

    update list for workers in a hive

--- src/hivewing_core/worker.clj
+++ src/hivewing_core/worker.clj
@@ -3,6 +3,22 @@
             [hivewing-core.core :refer [ensure-uuid]]
             [clojure.java.jdbc :as jdbc]))
 
+(defn worker-list
+  "Gets the list of worker uuids. It is paginated
+   and per_page is limited.  Returns the total as well"
+  [hive-uuid & params]
+  (let [params   (into {:page 1, :per-page 50} (apply hash-map  params))
+        per-page (if (> (:per-page params) 100) 100 (:per-page params))
+        page     (:page params)
+        ]
+    (println params)
+    (println hive-uuid)
+    (map :uuid (jdbc/query sql-db ["SELECT uuid FROM workers WHERE hive_uuid = ? LIMIT ? OFFSET ?"
+                        (ensure-uuid hive-uuid)
+                        per-page
+                        (* (- page 1) per-page)])
+    )))
+
 (defn worker-get
   "Get the data for worker record.  You pass in the worker via the worker uuid.  Returns the data
   as a hashmap."
@@ -10,8 +26,12 @@
   (jdbc/query sql-db ["SELECT * FROM workers WHERE uuid = ? LIMIT 1" (ensure-uuid worker-uuid)] :result-set-fn first))
 
 (defn worker-create
-  [parameters]
-  (first (jdbc/insert! sql-db :workers parameters)))
+  [{apiary-uuid :apiary_uuid hive-uuid :hive_uuid :as parameters}]
+
+  (let [clean-params (assoc parameters
+                            :apiary_uuid (ensure-uuid apiary-uuid)
+                            :hive_uuid (ensure-uuid hive-uuid))]
+    (first (jdbc/insert! sql-db :workers clean-params))))
 
 (defn worker-join-hive
   "Add a worker to a given hive. Removing it from a hive it was in before.
